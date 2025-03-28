5bdba3cb67fa768dc0ca4b022019f2473fe12c03
MAKSYM MURASHKO <122733440+LETO-NZT@users.noreply.github.com>
2025-03-28 19:38:58 +0100

    Update pixabay-api.js
    add my original api key pixabay.com

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const fetchImages = async (searchQuery, page = 1) => {
-  const API_KEY = '48797096-f4883239ab22667ebb957e7d3';
+  const API_KEY = '49580419-995b76e467c7e33d07d0013ac'; 
   const BASE_URL = 'https://pixabay.com/api/';
 
   try {
     const response = await axios.get(BASE_URL, {
       params: {
-        key: '48797096-f4883239ab22667ebb957e7d3',
+        key: '49580419-995b76e467c7e33d07d0013ac',
         q: searchQuery,
         image_type: 'photo',
         orientation: 'horizontal',
